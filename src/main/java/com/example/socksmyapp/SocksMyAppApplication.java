6f21dd817695cda0b28a5140a0ebec804f0f4190
Samsung <arslan.zakirov.1996@mail.ru>
2023-02-07 23:35:50 +0300

    первый коммит

---
+++ src/main/java/com/example/socksmyapp/SocksMyAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.socksmyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksMyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocksMyAppApplication.class, args);
+    }
+
+}
